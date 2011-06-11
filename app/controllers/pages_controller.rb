64ad4b6a7eff31a1109771e3cc2a92aebcd081af
Equivocal <ingebrigtsen@gmail.com>
2011-06-10 17:16:45 -0700

    Preliminary design + pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def news
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Grf::Application.routes.draw do
+  root :to => "pages#news"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
