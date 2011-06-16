bbabf0f75d6703f0147e6306b43951d29ebc24b5
Equivocal <ingebrigtsen@gmail.com>
2011-06-15 18:40:02 -0700

    Styling helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def is_current?(page_name)
+    if request.path_parameters[:action] == page_name || request.path_parameters[:controller] == page_name
+      "current"
+    end
+  end
 end
